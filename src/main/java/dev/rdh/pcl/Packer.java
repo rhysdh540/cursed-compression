9b8ea1ef24c3124899f0a11a7c8121b1811becd6
Rhys <98863820+rhysdh540@users.noreply.github.com>
2024-09-24 19:18:13 -0400

    nice comment

--- src/main/java/dev/rdh/pcl/Packer.java
+++ src/main/java/dev/rdh/pcl/Packer.java
@@ -15,9 +15,30 @@ public class Packer {
 		pack(root, output, Stream.of(args).skip(2).map(Paths::get));
 	}
 
+	/**
+	 * Concatenates all the files in the specified stream into the specified output.
+	 *
+	 * The output file is structured as such:
+	 * <ul>
+	 *     <li>for each item:
+	 *     <ol>
+	 *         <li>length of the name of the file (4 bytes)</li>
+	 *         <li>length of the contents of the file (4 bytes)</li>
+	 *         <li>name of the file</li>
+	 *         <li>contents of the file</li>
+	 *     </ol>
+	 *     </li>
+	 * </ul>
+	 *
+	 * @param root the root to relativize the paths to
+	 * @param output the file to write the packed bundle to
+	 * @param paths the paths that contain files to add to the bundle. May contain directories, they will be skipped.
+	 */
 	public static void pack(Path root, Path output, Stream<Path> paths) {
 		try(DataOutputStream out = new DataOutputStream(Files.newOutputStream(output))) {
 			paths.sorted().forEach(path -> {
+				if(Files.isDirectory(path)) return;
+
 				System.out.println("Packing: " + root.relativize(path));
 				byte[] name = root.relativize(path).toString().getBytes(StandardCharsets.UTF_8);
 				byte[] data = Files.readAllBytes(path);
